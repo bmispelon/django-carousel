5010e871bc58aa5aa9641cd5f4d51f6d42cb5bae
Baptiste Mispelon <bmispelon@gmail.com>
2010-11-09 19:19:03 +0100

    Added a position field for carousel elements and made it the default sort field.

--- carousel/models.py
+++ carousel/models.py
@@ -12,7 +12,7 @@ class Carousel(models.Model):
         verbose_name_plural = _('carousels')
     
     def get_elements(self):
-        return self.elements.all().order_by('name') # TODO: sort according to self.order
+        return self.elements.all()
 
 
 class CarouselElement(models.Model):
@@ -20,6 +20,7 @@ class CarouselElement(models.Model):
     name = models.CharField(_('name'), max_length=50)
     image = models.ImageField(_('image'), upload_to='carousel_uploads')
     url = models.URLField(_('URL'), blank=True)
+    position = models.PositiveIntegerField(_('position'), help_text=_('elements will be displayed in ascending order of position'), default=1)
     
     def __unicode__(self):
         return self.name
@@ -27,3 +28,4 @@ class CarouselElement(models.Model):
     class Meta:
         verbose_name = _('carousel element')
         verbose_name_plural = _('carousel elements')
+        ordering = ('position', 'name')
