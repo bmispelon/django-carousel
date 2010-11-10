62bafee990d6c541d63ca63cbfcf8fa538b03a08
Baptiste Mispelon <bmispelon@gmail.com>
2010-11-10 11:07:26 +0100

    Added different distributions for carousels.

--- carousel/models.py
+++ carousel/models.py
@@ -1,26 +1,69 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+import random
+from utils import weighted_shuffle
+from collections import defaultdict
 
 class Carousel(models.Model):
+    DISTRIB_SEQUENTIAL = 1
+    DISTRIB_RANDOM = 2
+    DISTRIB_WEIGHTED_RANDOM = 3
+    DISTRIB_CLUSTER_RANDOM = 4
+    DISTRIBUTIONS = (
+        (DISTRIB_SEQUENTIAL, _('sequential')),
+        (DISTRIB_RANDOM, _('random')),
+        (DISTRIB_WEIGHTED_RANDOM, _('weighted random')),
+        (DISTRIB_CLUSTER_RANDOM, _('cluster random'))
+    )
     name = models.CharField(_('name'), max_length=50, unique=True)
-    
+    distribution = models.PositiveSmallIntegerField(_('distribution'), choices=DISTRIBUTIONS, default=DISTRIB_SEQUENTIAL)
     def __unicode__(self):
         return self.name
     
     class Meta:
         verbose_name = _('carousel')
         verbose_name_plural = _('carousels')
+        ordering = ('name', )
     
     def get_elements(self):
-        return self.elements.all()
+        return {
+            self.DISTRIB_SEQUENTIAL: self._get_elements_sequential,
+            self.DISTRIB_RANDOM: self._get_elements_random,
+            self.DISTRIB_WEIGHTED_RANDOM: self._get_elements_weighted_random,
+            self.DISTRIB_CLUSTER_RANDOM: self._get_elements_cluster_random
+        }.get(self.distribution)()
+    
+    def _get_elements_sequential(self):
+        return self.elements.order_by('position')
+    
+    def _get_elements_random(self):
+        elements = list(self.elements.all()) # force evaluation of queryset
+        random.shuffle(elements)
+        return elements
+    
+    def _get_elements_weighted_random(self):
+        elements = list(self.elements.all()) # force evaluation of queryset
+        weighted_shuffle(elements, key_weight=lambda e: e.position)
+        return elements
+    
+    def _get_elements_cluster_random(self):
+        clusters = defaultdict(list)
+        for item in self.elements.all():
+            clusters[item.position].append(item)
+        for cluster in clusters.values():
+            random.shuffle(cluster)
+        
+        sorted_tuples = sorted(clusters.items(), key=lambda t: t[0])
+        return reduce(lambda x,t: x + t[1], sorted_tuples, [])
 
 
 class CarouselElement(models.Model):
+    POSITION_HELP_TEXT = _('The position of the element in the sequence or the weight of the element in the randomization process (depending on the carousel\'s distribution).')
     carousel = models.ForeignKey(Carousel, verbose_name=_('carousel'), related_name='elements')
     name = models.CharField(_('name'), max_length=50)
     image = models.ImageField(_('image'), upload_to='carousel_uploads')
     url = models.URLField(_('URL'), blank=True)
-    position = models.PositiveIntegerField(_('position'), help_text=_('elements will be displayed in ascending order of position'), default=1)
+    position = models.PositiveIntegerField(_('position'), default=1, help_text=POSITION_HELP_TEXT)
     
     def __unicode__(self):
         return self.name
---
+++ carousel/utils.py
@@ -0,0 +1,30 @@
+def weighted_pick(x, key_weight=None):
+    """Returns one element from the sequence x.
+    A "heavy" element is more likely to be picked.
+    The `key_weight` argument can be used to define how to access the element's weight.
+    By default, we assume x to be a sequence of (element, weight) tuples.
+    """
+    if key_weight is None:
+        key_weight = lambda t: t[1]
+    
+    total_weights = sum(key_weight(e) for e in x)
+    target = random.randint(0, total_weights)
+    for e in x:
+        target -= e[1]
+        if target <= 0:
+            return e[0]
+
+def weighted_shuffle(l, key_weight=None):
+    """Shuffles the sequence x in place.
+    Elements that are "heavier" will be more likely to be at the beginning.
+    The `key_weight` argument can be used to define how to access the element's weight.
+    By default, we assume x to be a sequence of (element, weight) tuples.
+    """
+    if key_weight is None:
+        key_weight = lambda t: t[1]
+    
+    weights = [(i, key_weight(e)) for e in enumerate(x)]
+    
+    for i in reversed(xrange(1, len(x))):
+        j = weighted_pick(weights[:i+1])
+        x[i], x[j] = x[j], x[i]
